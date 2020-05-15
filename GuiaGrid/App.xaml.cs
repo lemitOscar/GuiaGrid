19953070bad95fa0bcef618c3119863469ae87f1
oscar rodriguez <54603046+lemitOscar@users.noreply.github.com>
2020-05-14 20:01:26 -0500

    Agregar archivos de proyecto.

---
+++ GuiaGrid/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using GuiaGrid.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GuiaGrid
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PageGrid();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
